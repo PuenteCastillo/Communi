86aceaa5f80bf9bc31c5cc6dc30d303332c6a229
Eduardo Puente Castillo <puente@Eduardos-MBP.attlocal.net>
2023-03-26 23:47:08 -0700

    spotlight modle complete

--- src/components/pages/Spotlight.tsx
+++ src/components/pages/Spotlight.tsx
@@ -1,41 +1,60 @@
-import Image from "next/image";
 import Placeholder from "../../Images/barber_placholder.jpg";
+import Card from "./spotlight/Card";
+import Sign_up from "./spotlight/Sign_up";
+
+const Placeholder_img = {
+  src: Placeholder,
+  alt: "Random Image",
+};
+const cat = [
+  {
+    name: "Barber",
+    link: "#",
+  },
+  {
+    name: "Stylest",
+    link: "#",
+  },
+];
+
 export default function Spotlight(props) {
   return (
     <section id="spotlight" className="container">
       <div className="row">
-        <small>Spotlight</small>
-        <div className="col-md-8">
-          <div className="item service_card">
-            {/* // img */}
-            <Image
-              className="service_card_img"
-              src={Placeholder}
-              alt="Random Image"
-            />
-            {/* // category */}
-            <div className="categories">
-              <a href="#"> Barber </a>
-              <a href="#"> Stylest </a>
-            </div>
-            {/* // title */}
-            <h2>
-              The trick to getting more done is to have the freedom to roam
-              around
-            </h2>
-            {/* // description */}
-            <p>
-              Vel lectus vel velit pellentesque dignissim nec id magna. Cras
-              molestie ornare quam at semper. Proin a ipsum ex. Curabitur eu
-              venenatis justo. Nullam felis augue, imperdiet at sodales. Nullam
-              felis libero, congue quis ipsum et, lacinia maximus eros.
-              Vestibulum ante ipsum primis in faucibus.
-            </p>
-            {/* // ednorsments */}
-          </div>
+        <small className="mb-3">Spotlight</small>
+        <div className="col-md-12 col-lg-8">
+          <Card
+            title="The trick to getting more done is to have the freedom to roam around"
+            description="Vel lectus vel velit pellentesque dignissim nec id magna. Cras molestie ornare quam at semper. Proin a ipsum ex. Curabitur eu venenatis justo. Nullam felis augue, imperdiet at sodales. Nullam felis libero, congue quis ipsum et, lacinia maximus eros. Vestibulum ante ipsum primis in faucibus."
+            image={Placeholder_img}
+            categories={cat}
+            size="large"
+          />
         </div>
-        <div className="col-md-4">
-          <div className="signUpCard">sdf</div>
+        <div className="col-md-12 col-lg-4">
+          <div className="row">
+            <div className="col-12 col-md-6 col-lg-12">
+              <Sign_up />
+            </div>
+            <div className="col-12 col-md-6 col-lg-12">
+              <Card
+                title="The trick to getting more done is to "
+                //   description="Vel lectus vel velit pellentesque dignissim nec id magna. Cras molestie ornare quam at semper. Proin a ipsum ex. Curabitur eu venenatis justo. Nullam felis augue, imperdiet at sodales. Nullam felis libero, congue quis ipsum et, lacinia maximus eros. Vestibulum ante ipsum primis in faucibus."
+                image={Placeholder_img}
+                categories={cat}
+                size="small"
+              />
+            </div>
+            <div className="col-12 col-md-6 col-lg-12">
+              <Card
+                title="The trick to getting more done is to "
+                //   description="Vel lectus vel velit pellentesque dignissim nec id magna. Cras molestie ornare quam at semper. Proin a ipsum ex. Curabitur eu venenatis justo. Nullam felis augue, imperdiet at sodales. Nullam felis libero, congue quis ipsum et, lacinia maximus eros. Vestibulum ante ipsum primis in faucibus."
+                image={Placeholder_img}
+                categories={cat}
+                size="small"
+              />
+            </div>
+          </div>
         </div>
       </div>
     </section>
---
+++ src/components/pages/spotlight/Sign_up.tsx
@@ -0,0 +1,9 @@
+export default function Sign_up(props) {
+  return (
+    <div className="signUpCard">
+      <small>Sign Up </small>
+      <h2>Get all the latest posts delivered straight to your inbox.</h2>
+      <a href="#"> Join The Team </a>
+    </div>
+  );
+}
