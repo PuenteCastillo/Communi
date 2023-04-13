75f15474830e8299ea3d589188b327eebec90618
Eduardo Puente Castillo <puente@Eduardos-MBP.attlocal.net>
2023-04-13 15:55:13 -0700

    woking on landing page

---
+++ src/pages/landing.tsx
@@ -0,0 +1,22 @@
+import Base from "../layouts/Base";
+import Image from "next/image";
+import Logo from "../Images/logo.png";
+import Nav from "../components/header/Nav";
+import Carousel from "../components/global/Carousel";
+import Modal from "../components/global/Modal";
+import SaasHeader from "../components/header/SassHeader";
+import HowItWorks from "../components/global/HowItWorks";
+import MoreInfo from "../components/global/MoreInfo";
+
+export default function Home() {
+  return (
+    <Base>
+      <Nav />
+      <div className="page_content">
+        <SaasHeader />
+        <HowItWorks />
+        <MoreInfo />
+      </div>
+    </Base>
+  );
+}
